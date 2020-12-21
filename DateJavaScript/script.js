30107d1c8140208e058f92dd01bebebe6f4561de
Anderson <adersoosilvaa@gmail.com>
2020-12-21 20:05:10 -0300

    .

---
+++ DateJavaScript/script.js
@@ -0,0 +1,13 @@
+//teste 01 - retornando horarrio em milesegundos
+now = new Date;
+document.write("hora: "  + now.getTime()); 
+
+// var end, start; 
+
+// start = new Date(); 
+// for (var i = 0; i < 1000; i++) {
+//     Math.sqrt(i); 
+// }
+// end = new Date(); 
+
+// console.log('Operacao  took ' + (end.getTime() + start.getTime()) + ' msec'); 
