b4fc1b13d990901b2699e53642eb6beac95d74dd
Anderson <adersoosilvaa@gmail.com>
2021-02-01 20:02:41 -0300

    Evento de clique retornando uma imagem de um cachorro HTML + JS

---
+++ onclick-image-api/script.js
@@ -0,0 +1,9 @@
+const shibeApi = 'https://dog.ceo/api/breeds/image/random'; 
+
+function generateDog() {
+    fetch(shibeApi)
+     .then(res => res.json())
+     .then(data => document.getElementById('dog-wrapper').setAttribute('src', data.message))
+}; 
+
+generateDog(); 
